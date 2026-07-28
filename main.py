d2daa8cfc3c850d1713fe869f1d530a7f47d9610
Socrat Mohammad <127431597+socratmhamad@users.noreply.github.com>
2026-07-28 12:52:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import logging
 import subprocess
 import tempfile
 
-TOKEN = ''
+TOKEN = '6767447234:AAEXzLikJxLTvxcXM1YhMTKCIXlpmaFI2RM'
 DOWNLOAD_FOLDER = 'downloads'  # مجلد التنزيلات
 
 # إنشاء مجلد التنزيلات إذا لم يكن موجودًا
